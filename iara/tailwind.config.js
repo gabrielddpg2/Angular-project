33756fdd59a51eb7e9336c7f7cdf3ecab00000ae
gabrielzitto <gos2@cin.ufpe.br>
2025-06-11 13:14:57 -0300

    tailwind

---
+++ iara/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}", // Procura em todos os arquivos .html e .ts dentro da pasta src
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
